e16a6f432bacb16cd8fd61545ab6f61f0ecd7f75
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-09-10 23:36:03 +0200

    better bgmusic page

--- src/pages/BackgroundMusic.tsx
+++ src/pages/BackgroundMusic.tsx
@@ -1,30 +1,36 @@
+import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Ratio from 'react-bootstrap/Ratio'
 import Row from 'react-bootstrap/Row'
 
-// Replace these with the YouTube video IDs you want to feature
-const videoIds = [
-  'tQ-31hv3N5s?si=gHJjJ_mEisFBxy0W',
-  'i1zqFpHhIJ4?si=gQ5_XxFsqJAqGSrj',
-  'K7Ay3hxZx3A?si=2-u_ZPL5AdHrMFnk',
-  'bq7a_ktfYck?si=gZFsXOsWNlkzugSj',
+// Replace these with the YouTube videos you want to feature
+const videos = [
+  { id: 'tQ-31hv3N5s?si=gHJjJ_mEisFBxy0W', title: 'Nier Automata' },
+  { id: 'i1zqFpHhIJ4?si=gQ5_XxFsqJAqGSrj', title: 'Nier Replicant' },
+  { id: 'K7Ay3hxZx3A?si=2-u_ZPL5AdHrMFnk', title: 'Axiom Verge' },
+  { id: 'bq7a_ktfYck?si=gZFsXOsWNlkzugSj', title: 'Hyper Light Drifter' },
 ]
 
 function BackgroundMusic() {
   return (
     <div>
       <h1>Background Music</h1>
-      <Row xs={1} md={2} className="g-4 mt-2">
-        {videoIds.map((videoId) => (
-          <Col key={videoId}>
-            <Ratio aspectRatio="16x9">
-              <iframe
-                src={`https://www.youtube.com/embed/${videoId}`}
-                title={`YouTube video ${videoId}`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              />
-            </Ratio>
+      <Row xs={1} md={2} lg={3} className="g-4 mt-2">
+        {videos.map(({ id, title }) => (
+          <Col key={id}>
+            <Card className="video-card">
+              <Ratio aspectRatio="16x9">
+                <iframe
+                  src={`https://www.youtube.com/embed/${id}`}
+                  title={title}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                />
+              </Ratio>
+              <Card.Body>
+                <Card.Title className="text-center">{title}</Card.Title>
+              </Card.Body>
+            </Card>
           </Col>
         ))}
       </Row>
